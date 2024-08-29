9a1d3a59f877056b3d8d8ebede462ae9b8e863ea
koki.chin <545705507@qq.com>
2024-08-29 09:30:57 +0900

    Update api.js

--- nextcloud/api.js
+++ nextcloud/api.js
@@ -2,11 +2,11 @@ function parseDirectories(xmlString) {
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(xmlString, "application/xml");
   const responses = xmlDoc.getElementsByTagName("d:response");
-  const directoryPaths = responses.map(e=>{
+  const directoryPaths = responses.map(e => {
     const href = e.getElementsByTagName("d:href")[0].textContent;
     const resourceType = e.getElementsByTagName("d:resourcetype")[0];
     const isDirectory = resourceType.getElementsByTagName("d:collection").length > 0;
-    return { href, isDirectory};
+    return { href, isDirectory };
   });
   // for (let i = 1; i < responses.length; i++) {
   //   const href = responses[i].getElementsByTagName("d:href")[0].textContent;
@@ -18,8 +18,8 @@ function parseDirectories(xmlString) {
 };
 
 export default {
-  listDirectories(dir_path='/',USER_NAME = 'null',USER_PASS = 'null',
-                  base_url='/remote.php/dav/files') {
+  listDirectories(dir_path = '/', USER_NAME = 'null', USER_PASS = 'null',
+    base_url = '/remote.php/dav/files') {
     const propfindXML = `<?xml version="1.0"?>
     <d:propfind xmlns:d="DAV:">
       <d:prop>
@@ -27,12 +27,11 @@ export default {
       </d:prop>
     </d:propfind>`;
     return axios({
-      url:`${base_url}/${USER_NAME}${dir_path}`
-          .replaceAll('//','/').replaceAll('\\','/'),
+      url: `${base_url}/${USER_NAME}${dir_path}`
+        .replaceAll('//', '/').replaceAll('\\', '/'),
       method: 'PROPFIND',
       auth: {
-        username: USER_NAME,
-        password: USER_PASS
+        username: USER_NAME, password: USER_PASS
       },
       headers: {
         // 'Authorization': 'Basic ' + btoa(`${USER_NAME}:${USER_PASS}`),
@@ -42,15 +41,16 @@ export default {
       data: propfindXML
     }).then(response => parseDirectories(response.data));
   },
-  getFile(filePath, rangeStart, rangeEnd, USER_NAME = 'null',USER_PASS = 'null') {
-    var headers = {'Authorization': 'Basic ' + window.btoa(`${USER_NAME}:${USER_PASS}`)};
+  getFile(filePath, rangeStart, rangeEnd, USER_NAME = 'null', USER_PASS = 'null') {
+    var headers = { 'Authorization': 'Basic ' + window.btoa(`${USER_NAME}:${USER_PASS}`) };
     if (rangeStart !== undefined && rangeEnd !== undefined) {
       headers['Range'] = `bytes=${rangeStart}-${rangeEnd}`;
     } else if (rangeStart !== undefined) {
       headers['Range'] = `bytes=${rangeStart}-`;
     }
     // console.log(filePath);
-    return axios({method: 'GET',url: filePath,headers,
+    return axios({
+      method: 'GET', url: filePath, headers,
       responseType: 'blob'  // Important for binary files like images, videos, etc.
     });
     // .then(response => {      
